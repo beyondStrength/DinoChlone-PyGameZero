12b9e93ac754acff718b6408f6298ec5527a863e
beyondStrength <61022493+beyondStrength@users.noreply.github.com>
2021-12-27 18:49:34 -0300

    obstacles added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from pgzero import keyboard
+from random import randint
+from pgzero import keyboard, clock
 import pgzrun
 
 WIDTH = 1280
@@ -11,9 +12,20 @@ dino.y = HEIGHT/2
 speedY = 0
 currentSprite = 1
 
+obstacles = []
+
+def instantiateObstacle():
+    obstacle = Actor("obstacle"+str(randint(1,3)))
+    obstacle.pos = WIDTH+obstacle.width, HEIGHT/2
+    obstacles.append(obstacle)
+
+def moveObstacles():
+    for o in obstacles:
+        o.x -= 10
+
 def gravity():
     if dino.y < HEIGHT/2:
-        dino.y += 15
+        dino.y += 25
 
 def jump():
     global speedY
@@ -22,29 +34,37 @@ def jump():
         speedY += 2
     else:
         speedY = 0
-    if keyboard.w or keyboard.space:
+    if keyboard.w or keyboard.space or keyboard.up:
         if dino.y >= HEIGHT/2:
-            speedY = -35
+            speedY = -50
 
-def dinoRun():
+def animation():
     global currentSprite
-    if dino.y >= HEIGHT/2:
-        if currentSprite > 1:
+    if currentSprite == 2:
             currentSprite = 1
-        else:
-            currentSprite = 2
-    dino.image = f"dino{currentSprite}"
+    elif currentSprite == 1:
+        currentSprite = 2
+    if dino.y >= HEIGHT/2:
+        dino.image = f"dino{currentSprite}"
+    for o in obstacles:
+        if o.image != "obstacle3":
+            o.image = "obstacle"+str(currentSprite)
+    
 
 #Start
-clock.schedule_interval(dinoRun, 0.1)
+clock.schedule_interval(animation, 0.1)
+clock.schedule_interval(instantiateObstacle, 1)
 
 def update():
     gravity()
     jump()
+    moveObstacles()
 
 
 def draw():
     screen.fill((120,120,120))
     dino.draw()
+    for o in obstacles:
+        o.draw()
 
 pgzrun.go()
